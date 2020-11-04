f389bec3fd81b43623bcdb8b79abed4e2663ce4b
liuligang <javaweb_llg@163.com>
2020-11-04 10:20:33 +0800

    fix bug2

--- algorithm/EADP.py
+++ algorithm/EADP.py
@@ -32,7 +32,7 @@ G = nx.Graph()
 #                   (3, 5), (3, 6), (3, 7),
 #                   (4, 5), (4, 7), (4, 9), (4, 10),
 #                   (5, 7)], weight=1.0)
-G = nx.read_gml("../datasets/karate.gml", label="id")
+G = nx.read_gml("../datasets/football.gml", label="id")
 for edge in G.edges:
     G[edge[0]][edge[1]]['weight'] = 1.0
 
@@ -134,7 +134,7 @@ dist_martix, max_dist, ls_martix = init_dist_martix()
 
 # for i in range(len(dist_martix)):
 #     for j in range(len(dist_martix[i])):
-#         print dist_martix[i][j]
+#         print i, j, dist_martix[i][j]
 
 
 # 求网络的平均度
@@ -242,7 +242,7 @@ def init_all_nodes_info():
         # 当揉为最大的时候，取最大的dist
         if i == len(res) - 1:
             # todo ????? 这里应该是有问题的，不能取最大的，因为这里取最大的会导致下面的归一化的时候出现问题。暂定这里取出0试一试
-            # max_dist = 1.0
+            # max_dist = res[i-1].node_g
             res[i].node_g = max_dist
             all_node_g.append(max_dist)
         else:
@@ -289,9 +289,8 @@ def node_p_1_g_1_to_xy(nodes_info_list):
     for node_info in nodes_info_list:
         x.append(count)
         y.append(node_info.node_p_1)
-        # print node_info.node_p_1
         z.append(node_info.node_g_1)
-        # print node_info.node_g_1
+        print node_info.node, node_info.node_p_1, node_info.node_g_1
         count += 1
     return x, y, z
 
